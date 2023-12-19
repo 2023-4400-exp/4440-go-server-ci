a88adde1062389cef9dcdfc0b26b68c6793efd26
m20151 <m20151@g.metro-cit.ac.jp>
2023-12-19 14:11:20 +0900

    first commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:alpine AS builder
+
+COPY . /app
+WORKDIR /app
+
+RUN go get
+
+RUN go build -o server
+
+FROM alpine
+
+WORKDIR /app
+
+COPY --from=builder /app/server /app/
+
+CMD ["/app/server"]
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"github.com/labstack/echo"
+)
+
+func main(){
+	e := echo.New()
+
+	e.GET("/",Hello())
+	e.GET("/api/",ApiHelloGet())
+
+	e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "4440: Hello... ver.2")
+	}
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4440", "message":"HELLO..."})
+	}
+}
